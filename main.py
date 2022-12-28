aa861a21f568acd22621e1a5fc0c9552276e2985
Samuel Bruno <samuel.bruno@stud.unifi.it>
2022-12-28 17:48:19 +0100

    Creazione della hash table, classi e metodi

--- main.py
+++ main.py
@@ -1,15 +1,73 @@
-# This is a sample Python script.
+class ListNode:
+    def __init__(self, key, val):
+        self.pair = (key, val)
+        self.next = None
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class HashTable:
+
+    def __init__(self):
+        self.m = 1000
+        self.h = [None] * self.m    #Object to store keys/value Objects
+
+    def insert(self, key, value):
+        """
+        value will always be non-negative.
+        """
+        index = key % self.m
+        if self.h[index] is None:
+            self.h[index] = ListNode(key, value)
+        else:
+            cur = self.h[index]
+            while True:
+                if cur.pair[0] == key:
+                    cur.pair = (key, value)  # update
+                    return
+                if cur.next is None:
+                    break
+                cur = cur.next
+            cur.next = ListNode(key, value)
+
+    def find(self, key):
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        index = key % self.m
+        cur = self.h[index]
+        while cur:
+            if cur.pair[0] == key:
+                return cur.pair[1]
+            else:
+                cur = cur.next
+        return -1
+
+    def remove(self, key):
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        index = key % self.m
+        cur = prev = self.h[index]
+        if not cur:
+            return
+        if cur.pair[0] == key:
+            self.h[index] = cur.next
+        else:
+            cur = cur.next
+            while cur:
+                if cur.pair[0] == key:
+                    prev.next = cur.next
+                    break
+                else:
+                    cur, prev = cur.next, prev.next
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {piporittus}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    # obj = HashTable()
+    # obj.insert(key, value)
+    # param_2 = obj.find(key)
+    # obj.remove(key)
+    print("--------------- Hellooooo ---------------\n")
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
