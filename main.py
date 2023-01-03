9e4622ac854c7cf3e72bd6f0aa0ea3995e0d6d85
Samuel Bruno <samuel.bruno@stud.unifi.it>
2023-01-03 16:05:50 +0100

    Codice corretto e funzionante sia per divisione che per moltiplicazione

--- main.py
+++ main.py
@@ -1,72 +1,145 @@
-class ListNode:
-    def __init__(self, key, val):
-        self.pair = (key, val)
-        self.next = None
+import math
 
 
-class HashTable:
+class Node:
+    def __init__(self, key, value, next=None):
+        self.key = key
+        self.value = value
+        self.next = next
 
+
+class LinkedList:
     def __init__(self):
-        self.m = 1000
-        self.h = [None] * self.m    #Object to store keys/value Objects
+        self.head = None
+        self.size = 0
 
-    def insert(self, key, value):
-        """
-        value will always be non-negative.
-        """
-        index = key % self.m
-        if self.h[index] is None:
-            self.h[index] = ListNode(key, value)
-        else:
-            cur = self.h[index]
-            while True:
-                if cur.pair[0] == key:
-                    cur.pair = (key, value)  # update
-                    return
-                if cur.next is None:
-                    break
-                cur = cur.next
-            cur.next = ListNode(key, value)
+    def insertAtFirst(self, key, value):
+        self.head = Node(key, value, self.head)
+        self.size += 1
 
-    def find(self, key):
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        index = key % self.m
-        cur = self.h[index]
-        while cur:
-            if cur.pair[0] == key:
-                return cur.pair[1]
-            else:
-                cur = cur.next
-        return -1
+    def insertAtLast(self, key, value):
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(key, value)
+        self.size += 1
 
     def remove(self, key):
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        index = key % self.m
-        cur = prev = self.h[index]
-        if not cur:
-            return
-        if cur.pair[0] == key:
-            self.h[index] = cur.next
+        current = self.head
+        previous = None
+        if current.key == key:
+            self.head = current.next
         else:
-            cur = cur.next
-            while cur:
-                if cur.pair[0] == key:
-                    prev.next = cur.next
-                    break
-                else:
-                    cur, prev = cur.next, prev.next
+            while current.next:
+                previous = current
+                current = current.next
+                if current.key == key:
+                    previous.next = current.next
+            self.size -= 1
+
+    def find(self, key):
+        current = self.head
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+
+    def printAll(self):
+        current = self.head
+        while current:
+            print(current.key, current.value)
+            current = current.next
+
+
+class HashTableMultiplication:
+    def __init__(self):
+        self.m = 128  # Scelgo un numero che sia una potenza di 2
+        self.h = [None] * self.m
+        self.A = ((math.sqrt(5) - 1) / 2)
+
+    def insert(self, key, value):
+        index = self.m * math.floor((key * self.A) % 1)
+        if self.h[index] is None:
+            newLinkedList = LinkedList()
+            newLinkedList.insertAtFirst(key, value)
+            self.h[index] = newLinkedList
+        else:
+            self.h[index].insertAtLast(key, value)
+
+    def get(self, key):
+        index = self.m * math.floor((key * self.A) % 1)
+        if self.h[index] is not None:
+            return self.h[index].find(key)
+
+    def delete(self, key):
+        index = self.m * math.floor((key * self.A) % 1)
+        if self.h[index] is not None:
+            self.h[index].remove(key)
+
+    def printAll(self):
+        for i in self.h:
+            if i is not None:
+                i.printAll()
+
+
+class HashTableDivision:
+    def __init__(self):
+        self.m = 101  # Scelgo un numero primo che non sia una potenza di 2
+        self.h = [None] * self.m
+
+    def insert(self, key, value):
+        index = key % self.m
+        if self.h[index] is None:
+            newLinkedList = LinkedList()
+            newLinkedList.insertAtFirst(key, value)
+            self.h[index] = newLinkedList
+        else:
+            self.h[index].insertAtLast(key, value)
+
+    def get(self, key):
+        index = key % self.m
+        if self.h[index] is not None:
+            return self.h[index].find(key)
+
+    def delete(self, key):
+        index = key % self.m
+        if self.h[index] is not None:
+            self.h[index].remove(key)
+
+    def printAll(self):
+        for i in self.h:
+            if i is not None:
+                i.printAll()
 
 
 def main():
-    # obj = HashTable()
-    # obj.insert(key, value)
-    # param_2 = obj.find(key)
-    # obj.remove(key)
-    print("--------------- Hellooooo ---------------\n")
+    div = HashTableDivision()
+    div.insert(0, 98)
+    div.insert(0, 99)
+    div.insert(0, 63)
+    div.insert(3, 5)
+    param_2 = div.get(0)
+    # print("Valore cercato: ", param_2)
+    param_3 = div.get(1)
+    # print("Valore cercato: ", param_3)
+    div.delete(3)
+    div.delete(0)
+    div.delete(0)
+    # div.printAll()
+
+    mul = HashTableMultiplication()
+    mul.insert(0, 98)
+    mul.insert(0, 99)
+    mul.insert(0, 63)
+    mul.insert(3, 5)
+    param_2 = mul.get(0)
+    # print("Valore cercato: ", param_2)
+    param_3 = mul.get(1)
+    # print("Valore cercato: ", param_3)
+    mul.delete(3)
+    mul.delete(0)
+    mul.delete(0)
+    # mul.printAll()
 
 
 if __name__ == '__main__':
